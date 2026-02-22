047ee47de52466443b9e55a4bc8614ea19cb8fb2
William <williamcpereira89@gmail.com>
2026-02-22 02:02:24 -0300

    configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
+    strictPort: true,
+    allowedHosts: ["wilho.com.br"],
+    hmr: {
+      protocol: "wss",
+      host: "wilho.com.br",
+    },
   },
 });
